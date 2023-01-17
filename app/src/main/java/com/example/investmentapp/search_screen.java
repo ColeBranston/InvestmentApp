c5cfe1fd98f15ecc21b6a1866f685aefd44214c7
Cole Branston <colebranston@gmail.com>
2023-01-17 10:43:18 -0500

    Added the search page

--- app/src/main/java/com/example/investmentapp/search_screen.java
+++ app/src/main/java/com/example/investmentapp/search_screen.java
@@ -1,23 +1,18 @@
 package com.example.investmentapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class search_screen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         View v = this.getWindow().getDecorView();
         v.setSystemUiVisibility(View.INVISIBLE);
-    }
-    public void LaunchSearch(View v){
-        Intent i = new Intent(this, search_screen.class);
-        setContentView(R.layout.search_screen);
+
     }
 }
